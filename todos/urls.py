8289df87058d0b155efc256aba579f4064cc8345
Talkhestani <SirAlidadi@hotmail.com>
2024-07-24 00:58:26 +0330

    :alien: complete todo project

--- todos/urls.py
+++ todos/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "todos"
 urlpatterns = [
     path("", views.todo_list, name="todo-list"),
-    path("create/", views.todo_create, name="todo-create")
+    path("create/", views.todo_create, name="todo-create"),
+    path("done/<int:todo_id>/", views.todo_done_undone, name="todo-done-undone"),
+    path("delete/<int:todo_id>/", views.todo_delete, name="todo-delete"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 from todos.forms import TodoForm
 from todos.models import Todo
 
 
 # Create your views here.
+@require_http_methods(["GET"])
 def todo_list(request):
     context = {
         'todos': Todo.objects.all(),
@@ -12,6 +15,7 @@ def todo_list(request):
     return render(request, context=context, template_name="todos/todo-list.html")
 
 
+@require_http_methods(["POST"])
 def todo_create(request):
     form = TodoForm(data=request.POST)
 
@@ -21,3 +25,26 @@ def todo_create(request):
             'todo': todo
         }
         return render(request, template_name="todos/todo-list.html#todo-list-item", context=context)
+
+
+@require_http_methods(["POST"])
+def todo_done_undone(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.done = not todo.done
+    todo.save()
+
+    context = {
+        'todo': todo
+    }
+
+    return render(request, template_name="todos/todo-list.html#todo-list-item", context=context)
+
+
+@require_http_methods(["DELETE"])
+def todo_delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.delete()
+    response = HttpResponse(status=204)
+    response['HX-Trigger'] = 'todo-delete'
+    return response
+
